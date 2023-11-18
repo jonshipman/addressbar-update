52e4790364599aa6557a866ddb1c59f13e18c15d
Jon Shipman <jon@jonshipman.com>
2023-11-18 07:26:17 -0600

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+
+	let searchTerm = '';
+	let input: HTMLInputElement;
+
+	$: browser && updateAddressBar(searchTerm);
+
+	function updateAddressBar(s: string) {
+		setTimeout(() => {
+			goto(`?q=${s}`).then(handleFocus);
+		});
+	}
+
+	function handleFocus() {
+		setTimeout(() => {
+			input.focus();
+			input.setSelectionRange(1000, 1000);
+		});
+	}
+</script>
+
+<input bind:value={searchTerm} bind:this={input} />
